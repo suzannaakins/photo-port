b83ea2d84c36db1cf42de064fc83ed61cda2e5f6
Garlic Fiend <suzannaakins@gmail.com>
2020-12-29 11:48:40 -0800

    contact form uses controlled components to capture user data

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
     {
-        name: "commercial",
-        description: "Photos of grocery stores, food trucks, and other commercial projects",
+      name: "commercial",
+      description: "Photos of grocery stores, food trucks, and other commercial projects",
     },
     {
-        name: "portraits",
-        description: "Portraits of people in my life"
+      name: "portraits",
+      description: "Portraits of people in my life"
     },
     {
-        name: "food",
-        description: "Delicious delicacies"
+      name: "food",
+      description: "Delicious delicacies"
     },
     {
-        name: "landscape",
-        description: "Fields, farmhouses, waterfalls, and the beauty of nature"
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature"
     },
-]);
+  ]);
 
-const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
     <div>
@@ -33,6 +34,7 @@ const [currentCategory, setCurrentCategory] = useState(categories[0]);
         currentCategory={currentCategory}
       ></Nav>
       <main>
+        <ContactForm></ContactForm>
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm() {
+    //set initial values of the form state to empty strings, to clear the input fields when the component loads
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
+
     return (
         <section>
             <h1>Contact Me</h1>
             <form id="contact-form">
-                //name input
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
                 </div>
-                //email input
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" defaultValue={email} onChange={handleChange} name="email" />
                 </div>
-                // message text area
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea rows="5" defaultValue={message} onChange={handleChange} name="message" />
                 </div>
                 <button type="submit">Submit</button>
             </form>
