a109243c8796d31ce6c676b507a5203548f7b2c7
Garlic Fiend <suzannaakins@gmail.com>
2020-12-29 11:52:43 -0800

    submit function set up for back end developers

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -9,10 +9,15 @@ function ContactForm() {
         setFormState({...formState, [e.target.name]: e.target.value })
     }
 
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
     return (
         <section>
             <h1>Contact Me</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
                     <input type="text" defaultValue={name} onChange={handleChange} name="name" />
