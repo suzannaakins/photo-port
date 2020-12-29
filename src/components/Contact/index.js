5d5a378f996fab6c92dd81a9117b76d817a45c66
Garlic Fiend <suzannaakins@gmail.com>
2020-12-29 12:22:46 -0800

    render errorMessage to the user on the page with a conditional statement

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -37,16 +37,23 @@ function ContactForm() {
             <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
+                    <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" defaultValue={email} onChange={handleChange} name="email" />
+                    <input type="email" defaultValue={email} onBlur={handleChange} name="email" />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea rows="5" defaultValue={message} onChange={handleChange} name="message" />
+                    <textarea rows="5" defaultValue={message} onBlur={handleChange} name="message" />
                 </div>
+
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
+
                 <button type="submit">Submit</button>
             </form>
         </section>
